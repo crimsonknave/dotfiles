6e472c24cf9a948441322c353020a7fbaa11cd53
crimsonknave <crimsonknave@gmail.com>
2024-10-25 18:00:39 -0400

    Working on hyperfocus helper script

--- hyperfocus/game-chunk.sh
+++ hyperfocus/game-chunk.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/bash
 
 game=$@
-kill_time=30
-save_time=1
-sleep_time=$(($kill_time-$save_time))
 unit="m"
+kill_time=30
 save_message="                                                    $sleep_time$unit have passed. You have $save_time$unit  to save"
 launch_message="                                                                              Playing for $kill_time$unit."
 swaynag_settings='-o DP-1 -y overlay --message-padding 24 --button-padding 19 -f "Monospace 24"'
 
+duration_message () {
+  echo "--button-dismiss $1m" "echo '$1' > /tmp/game-chunk-time"
+}
+
+echo $(duration_message 6)
+
 day=$(date +%a)
 hour=$(date +%H)
+hour=19
 rm -rf /tmp/vacation-true
+echo $kill_time > /tmp/game-chunk-time
 case "$day-$hour" in
     (*-1[89])        ;&
     (*-2[01]) ;&
     (Sat-1[1234567]) ;&
     (Sun-1[1234567]) 
-        swaynag -m "$launch_message" -s Start! $swaynag_settings;;
+      echo sswaynag -m "$launch_message" --button-dismiss "5m" 'echo "5" > /tmp/game-chunk-time' -s Start! $swaynag_settings
+      echo aswaynag -m "$launch_message" $(duration_message 5) -s Start! $swaynag_settings
+      swaynag -m "$launch_message" $(duration_message 5) -s Start! $swaynag_settings;;
     (*-1[1234567]) 
         swaynag -m "Are you on vacation?" -s No --button-dismiss Yes 'touch /tmp/vacation-true' --background F0B27A --button-background F0B27A --text 1C2833 --button-text 1C2833 $swaynag_settings -d
         sleep 1
         if [ -f /tmp/vacation-true ]; then
           echo "On vacation, playing during the day on a weekday is allowed."
+          swaynag -m "$launch_message" -s Start! $swaynag_settings
         else
           echo "Not on vacation, no playing."
           exit 1
@@ -32,6 +41,10 @@ case "$day-$hour" in
         exit 1;
 esac
 
+cat /tmp/game-chunk-time
+kill_time=$(cat /tmp/game-chunk-time)
+save_time=1
+sleep_time=$(($kill_time-$save_time))
 echo "Killing game after $kill_time$unit"
 echo "Warning to save after $sleep_time$unit"
 (sleep $sleep_time$unit && swaynag -m "$save_message" -s Saved! $swaynag_settings; echo "Save message removed") &
